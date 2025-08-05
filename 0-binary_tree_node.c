97524245bc828ea21ec09356536bfa11506b0cf6
ErickGaiero <erickgaiero15@gmail.com>
2025-08-05 16:48:49 +0000

    Exercise 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ *
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+	binary_tree_t *newNode = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return (newNode);
+
+}
