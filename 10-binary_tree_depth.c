6a2fb291b5d7f82bdba80218d00b13a2751f411e
ErickGaiero <erickgaiero15@gmail.com>
2025-08-08 19:12:23 +0000

    Exercise 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,9 +8,9 @@
  * Return: Depth of the node
  */
 
-int binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int depth = 0;
+	size_t depth = 0;
 
 	if (tree == NULL)
 		return (0);
