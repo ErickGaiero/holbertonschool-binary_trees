e9672f45d3cdd57e5491ac2c184490f3e8d1471c
ErickGaiero <erickgaiero15@gmail.com>
2025-08-10 06:30:58 +0000

    Exercise 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node
+ * Return: Pointer to the uncle node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+	binary_tree_t *grandparent;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	grandparent = parent->parent;
+
+	if (grandparent == NULL)
+		return (NULL);
+
+	if (grandparent->left == parent)
+		return (grandparent->right);
+
+	return (grandparent->left);
+}
