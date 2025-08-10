228efec81dfb6c10bee58db533e60c70a6fe73f0
Agustin Vidal <agustinvm88@gmail.com>
2025-08-10 23:58:16 +0000

    Add 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,6 +3,56 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - Mide el tamano de un arbol binario
+ *
+ * @tree: Puntero al nodo raiz del arbol
+ *
+ * Return: Tamano del arbol, o 0 si es NULL
+ *
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size;
+	/* Caso base: arbol vacio */
+	if (tree == NULL)
+		return (0);
+	/* Calcular tamano del subarbol izquierdo */
+	left_size = binary_tree_size(tree->left);
+	/* Calcular tamano del subarbol derecho */
+	right_size = binary_tree_size(tree->right);
+	/* Suma ambos tamanos + 1 por el nodo actual */
+	return (left_size + right_size + 1);
+}
+
+/**
+ * binary_tree_height - Mide la altura de un arbol binario
+ *
+ * @tree: Puntero a la raiz del arbol
+ *
+ * Return: Altura del arbol, o 0 si tree es NULL
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h, right_h;
+
+	if (tree == NULL)
+		return (0);
+	/* Si no tiene hijos, altura es 0 */
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	/* Calcula altura de cada subarbol */
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	/* Retornar la mayor altura + 1 (por la arista actual) */
+	if (left_h > right_h)
+		return (left_h + 1);
+	else
+		return (right_h + 1);
+}
+
+
+/**
  * binary_tree_is_perfect - Verifica si un arbol binario es perfecto
  *
  * @tree: Puntero al nodo raiz del arbol
